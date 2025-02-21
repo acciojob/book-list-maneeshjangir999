c80ccf2f9c315e9730f416f9ad8cf99d26cc5eb5
Maneesh Jangir <122728056+maneeshjangir999@users.noreply.github.com>
2025-02-21 20:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,12 +14,27 @@ let addBookList = ()=>{
 	let tabTitle = document.createElement("td")
 	let tabAuthor = document.createElement("td")
 	let tabIsbn = document.createElement("td")
+    var removeBtn = document.createElement("button")
+	removeBtn.classList.add("delete")
+	removeBtn.innerText = "Delete"
+	removeBtn.style.backgroundColor = "red"
+	removeBtn.style.color = "white"
+	removeBtn.style.padding = "5px 13px"
+	removeBtn.style.borderRadius = "10px"
+	removeBtn.style.border = "none"
+	
+	
 	tabRow.appendChild(tabTitle)
 	tabRow.appendChild(tabAuthor)
 	tabRow.appendChild(tabIsbn)
+	tabRow.appendChild(removeBtn)
 	table.appendChild(tabRow)
 	tabTitle.innerText = firstInput.value
 	tabAuthor.innerText = secInput.value
 	tabIsbn.innerText = thirInput.value
 }
+let remBookList = ()=>{
+	
+}
 btn.addEventListener("click",addBookList)
+removeBtn.addEventListener("click",remBookList)
