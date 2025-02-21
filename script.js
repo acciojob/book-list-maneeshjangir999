9498405bee90826843496d836de38bfccb66ebef
Maneesh Jangir <122728056+maneeshjangir999@users.noreply.github.com>
2025-02-21 13:12:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+let btn = document.getElementById("submit")
+let table = document.querySelector(".table")
+let firstInput = document.getElementById("title")
+let secInput = document.getElementById("author")
+let thirInput = document.getElementById("isbn")
+let title 
+let author
+let isbn
+let addBookList = ()=>{
+	title = firstInput.value
+	author = secInput.value
+	isbn = thirInput.value
+	let tabRow = document.createElement("tr")
+	let tabTitle = document.createElement("td")
+	let tabAuthor = document.createElement("td")
+	let tabIsbn = document.createElement("td")
+	tabRow.appendChild(tabTitle)
+	tabRow.appendChild(tabAuthor)
+	tabRow.appendChild(tabIsbn)
+	table.appendChild(tabRow)
+	tabTitle.innerText = firstInput.value
+	tabAuthor.innerText = secInput.value
+	tabIsbn.innerText = thirInput.value
+}
+btn.addEventListener("click",addBookList)
